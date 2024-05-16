d71f30f98af43dab184eb681be0a114a1600e378
BrunoVital <bruvital2010@hotmail.com>
2024-05-16 17:10:29 -0300

    1

--- Java-Anatomia-classes/src/MinhaClasse.java
+++ Java-Anatomia-classes/src/MinhaClasse.java
@@ -1,3 +1,16 @@
 public class MinhaClasse {
     
+public static void main (String [] args) {
+    
+    String primeiroNome = "Bruno";
+    String segundoNome = "Vital";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
 }
