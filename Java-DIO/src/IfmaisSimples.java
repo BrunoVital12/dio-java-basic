9d9aed851d234277a03ac0c1da6d51cb061eebe4
BrunoVital <bruvital2010@hotmail.com>
2024-05-21 10:19:17 -0300

    att

---
+++ Java-DIO/src/IfmaisSimples.java
@@ -0,0 +1,18 @@
+public class IfmaisSimples {
+
+    public static void main(String[] args){
+        int a, b;
+        a = 6;
+        b = 5;
+
+        //if a == b
+        //resultado = 1
+        //else
+        //resultado = 0
+        int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+
+    }
+    
+}
